8d09435749f779f74ffb2ee52ad460d12c9cdefd
Vitaliy Dymna <vitaliy.dymna@xcentium.com>
2018-11-09 10:12:12 -0600

    Disable bottom arrows after click event
    Added the code back to disable bottom navigation arrows for a short period after a click event

--- AllianceAssociationBank.Crm/Scripts/app/navigationWidgets.js
+++ AllianceAssociationBank.Crm/Scripts/app/navigationWidgets.js
@@ -20,46 +20,47 @@
         });
     };
 
-    var disableTabBottonNavigationButton = function () {
-        $previousTab = $(".nav-tabs > .active").prev("a");
-        $nextTab = $(".nav-tabs > .active").next("a");
+    //var disableTabBottonNavigationButton = function () {
+    //    $previousTab = $(".nav-tabs > .active").prev("a");
+    //    $nextTab = $(".nav-tabs > .active").next("a");
 
-        if ($previousTab.length === 0) {
-            $("#btn-change-tab-left").attr("disabled", true);
-        } else {
-            $("#btn-change-tab-left").removeAttr("disabled");
-        }
+    //    if ($previousTab.length === 0) {
+    //        $("#btn-change-tab-left").attr("disabled", true);
+    //    } else {
+    //        $("#btn-change-tab-left").removeAttr("disabled");
+    //    }
 
-        if ($nextTab.length === 0) {
-            $("#btn-change-tab-right").attr("disabled", true);
-        } else {
-            $("#btn-change-tab-right").removeAttr("disabled");
-        }
-    }
+    //    if ($nextTab.length === 0) {
+    //        $("#btn-change-tab-right").attr("disabled", true);
+    //    } else {
+    //        $("#btn-change-tab-right").removeAttr("disabled");
+    //    }
+    //}
 
     var bindTabBottomNavigationEvents = function () {
-        $(".nav-tabs").on("shown.bs.tab", disableTabBottonNavigationButton);
+        //$(".nav-tabs").on("shown.bs.tab", disableTabBottonNavigationButton);
 
         $("#btn-change-tab-left").on("click", function () {
             // disable the button a for short period so click event can be fully handled
-            //this.disabled = true;
+            this.disabled = true;
             $(".nav-tabs > .active").prev("a").trigger("click");
-            //enableElementAfterTimeout(this, 200);
+            enableElementAfterTimeout(this, 200);
         });
 
         $("#btn-change-tab-right").on("click", function () {
-            //this.disabled = true;
+            // disable the button a for short period so click event can be fully handled
+            this.disabled = true;
             $(".nav-tabs > .active").next("a").trigger("click");
-            //enableElementAfterTimeout(this, 200);
+            enableElementAfterTimeout(this, 200);
         });
     };
 
-    //var enableElementAfterTimeout = function (e, timeout) {
-    //    var enableElement = function (e) {
-    //        e.removeAttribute("disabled");
-    //    };
-    //    setTimeout(enableElement, timeout, e);
-    //}
+    var enableElementAfterTimeout = function (e, timeout) {
+        var enableElement = function (e) {
+            e.removeAttribute("disabled");
+        };
+        setTimeout(enableElement, timeout, e);
+    }
 
     var bindScreenOverlayFadeOut = function () {
         $(".screen-overlay").on("click", function (event) {
