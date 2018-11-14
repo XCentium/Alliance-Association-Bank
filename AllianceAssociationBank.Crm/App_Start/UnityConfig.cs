dd957fea1fc31682d8abf92703252df9177b5d94
Vitaliy Dymna <vitaliy.dymna@xcentium.com>
2018-11-14 09:35:34 -0600

    Principal context type change for local dev

--- AllianceAssociationBank.Crm/App_Start/UnityConfig.cs
+++ AllianceAssociationBank.Crm/App_Start/UnityConfig.cs
@@ -75,7 +75,7 @@ namespace AllianceAssociationBank.Crm
             container.RegisterType<PrincipalContext>
             (
                 new HierarchicalLifetimeManager(),
-                new InjectionFactory(c => new PrincipalContext(ContextType.Domain))
+                new InjectionFactory(c => GetPrincipalContext())
             );
             container.RegisterType<IAuthenticationManager>
             (
@@ -88,13 +88,13 @@ namespace AllianceAssociationBank.Crm
             container.RegisterInstance<IMapper>(CrmAutoMapperProfile.GetMapper());
         }
 
-//        private static PrincipalContext GetPrincipalContext()
-//        {
-//#if (!DEBUG)
-//            return new PrincipalContext(ContextType.Domain);
-//#else
-//            return new PrincipalContext(ContextType.Machine);
-//#endif
-//        }
+        private static PrincipalContext GetPrincipalContext()
+        {
+#if (!DEBUG)
+            return new PrincipalContext(ContextType.Domain);
+#else
+            return new PrincipalContext(ContextType.Machine);
+#endif
+        }
     }
 }
