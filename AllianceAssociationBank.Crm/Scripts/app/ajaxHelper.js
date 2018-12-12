c455325ae5c307a949387a94807888e3c93595b8
Vitaliy Dymna <vitaliy.dymna@xcentium.com>
2018-12-12 11:36:15 -0600

    Small JS change

--- AllianceAssociationBank.Crm/Scripts/app/ajaxHelper.js
+++ AllianceAssociationBank.Crm/Scripts/app/ajaxHelper.js
@@ -10,11 +10,6 @@
     var hideLoadingAnimation = function () {
         $loading = $(LOADING_ELEMENT_CONTAINER);
         $loading.hide("fade", 1000);
-        //var hideElement = function (element) {
-        //    $loadingElement = $($(element).data("loading-element-id"));
-        //    $loadingElement.hide("fade", 1000);
-        //};
-        //setTimeout(hideElement, 200, this);
     };
 
     return {
